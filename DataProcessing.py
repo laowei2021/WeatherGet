e6a29414c346d4b9015f83527cb9b832f8cb6f1c
laowei2021 <99231932+laowei2021@users.noreply.github.com>
2025-03-28 21:02:09 +0800

    Add files via upload

---
+++ DataProcessing.py
@@ -0,0 +1,187 @@
+﻿import requests,json
+from bs4 import BeautifulSoup
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import time
+from PIL import Image
+import datetime
+   
+#中国天气网天津地区地址
+url_7day = "https://www.weather.com.cn/weather/"
+url_api = "http://t.weather.sojson.com/api/weather/city/"
+url_tj_1day = "https://www.weather.com.cn/weather1d/101030100.shtml"
+url_tj_7day = "https://www.weather.com.cn/weather/101030100.shtml"
+url_tj_api="http://t.weather.sojson.com/api/weather/city/101030100"
+#获取网页内容
+def get_html(url):
+    try:
+        #设置请求头
+        headers = { 
+	    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' 
+	    } 
+
+        reply = requests.get(url,headers=headers,timeout=30)
+        
+        reply.raise_for_status()
+        return reply.content
+    except requests.RequestException as e:
+        print(e)
+        return None
+
+#元素解析并获取天气数据
+def parse_html_1day(code):
+
+    #这里使用api，获取更多的信息
+    '''
+    if not html:
+        return {}
+
+    soup = BeautifulSoup(html,"html.parser")
+
+    
+
+    #天气数据
+    data = {}
+
+    #城市
+    city = soup.find('div',class_="crumbs fl").find('a').text
+    data["city"] = city
+
+    #天气
+    weather = soup.find('p',class_="wea").title
+    data["weather"] = weather
+
+    #温度
+    print(soup.find_all('div',class_="tem"))
+    temperature = soup.find('div',class_="tem").find(id='span').get_text()
+    data["temperature"] = temperature
+
+    #风速风向
+    winddir = soup.find('div',class_="zs w").find(id='span').get_text()
+    windforce = soup.find('div',class_="zs w").find('em').text
+    data["wind"] = f"{winddir} {windforce}"
+    
+    return data
+    '''
+    #通过国家气象局api获取天气信息
+    weather_dict={}
+    reply = requests.get(url_api+str(code))
+    d = reply.json()
+    #若访问成功，解析天气信息
+    if d["status"] == 200:
+        weather_dict["city"] = d["cityInfo"]["city"]
+        weather_dict["pm25"]=d["data"]["pm25"]
+        weather_dict["pm10"]=d["data"]["pm10"]
+        weather_dict["weather"] = d["data"]["forecast"][0]["type"]
+        weather_dict["high"]=d["data"]["forecast"][0]["high"]
+        weather_dict["low"]=d["data"]["forecast"][0]["low"]
+        weather_dict["fx"]=d["data"]["forecast"][0]["fx"]
+        weather_dict["fl"]=d["data"]["forecast"][0]["fl"]
+        weather_dict["moicon"]=d["data"]["shidu"]
+        weather_dict["air"]=d["data"]["quality"]
+        weather_dict["sunrise"]=d["data"]["forecast"][0]["sunrise"]
+        weather_dict["sunset"]=d["data"]["forecast"][0]["sunset"]
+        weather_dict["aqi"]=d["data"]["forecast"][0]["aqi"]
+        weather_dict["tip"]=d["data"]["forecast"][0]["notice"]
+
+    else:
+        return {}
+    print(weather_dict)
+    return weather_dict
+
+#七日天气情况获取
+def parse_html_7day(html):
+     if not html:
+         return []
+     #使用bs4解析网页
+     soup = BeautifulSoup(html,"html.parser")
+
+     weatherdata = []
+     #解析天气信息
+     for item in soup.find_all('li',class_="sky"):
+         date = item.find('h1').text
+         weather = item.find('p',class_="wea").text
+         temperature = item.find('p',class_="tem").find('i').text
+         weatherdata.append({"date":date,"weather":weather,"temperature":temperature[:-1]})
+
+     return weatherdata
+
+def get_weeks():
+    
+
+    today = datetime.date.today()
+    base_year = today.year  # 获取今天的年份
+    start_date = datetime.date(base_year-1, 12, 31)  # 设置基准日期为去年的12月31日
+    current_date = datetime.date.today()  # 得到今天的时间点
+
+    # 计算从基准日期到今天的时间跨度
+    delta = current_date - start_date
+    total_days = delta.days  # 获取时间跨度天数
+
+    # 计算星期几（0代表周一）
+    week_number = total_days // 7 + 1  # 周一为0，周二是1
+    return base_year,week_number
+
+def data_7day_to_image(citycode):
+    plt.rcParams['font.sans-serif'] = ['SimHei'] # 设置字体为黑体
+    plt.rcParams['axes.unicode_minus'] = False # 解决负号显示为方块的问题
+    plt.figure(figsize=(6.0,5.0))#设置图片尺寸
+    plt.xlabel("时间/天")#设置横坐标
+    plt.ylabel("温度/摄氏度")#设置纵坐标
+    plt.grid(True)#启用网格
+
+    weatherdata=parse_html_7day(get_html(url_7day+str(citycode)+".shtml"))#获取7日数据
+
+    #初始化点数据
+    days=[]
+    temperatures=[]
+    
+    #处理点坐标数据
+    for i in range(len(weatherdata)):
+        days.append(i)
+        temperatures.append(int(weatherdata[i]["temperature"]))
+    #print(days,temperatures)
+    #for i in range(len(days)):
+    #    plt.plot(days[i],temperatures[i])
+    plt.plot(days,temperatures)#展示点坐标数据
+    plt.savefig(".\\tempimages\\file.png")#保存为PNG图片
+
+    year,week = get_weeks()#获取周数数据
+ 
+    image = Image.open(".\\tempimages\\file.png")#转换格式
+    image.save(".\\images\\"+str(year)+"-"+str(week)+"-"+str(citycode)+".gif")
+    return ".\\images\\"+str(year)+"-"+str(week)+"-"+str(citycode)+".gif"
+
+
+'''
+if __name__ == "__main__":
+    #plt.title("TestTemperature")
+    plt.rcParams['font.sans-serif'] = ['SimHei'] # 设置字体为黑体
+    plt.rcParams['axes.unicode_minus'] = False # 解决负号显示为方块的问题
+    plt.figure(figsize=(6.0,5.0))
+    plt.xlabel("时间/天")
+    plt.ylabel("温度/摄氏度")
+    plt.grid(True)
+
+    weatherdata=parse_html_7day(get_html(url_tj_7day))
+    days=[]
+    temperatures=[]
+    a=[]
+    for i in range(len(weatherdata)):
+        days.append(i)
+        temperatures.append(int(weatherdata[i]["temperature"]))
+    print(days,temperatures)
+    #for i in range(len(days)):
+    #    plt.plot(days[i],temperatures[i])
+    plt.plot(days,temperatures)
+
+    plt.savefig(".\\tempimages\\file.png")
+
+    year,week = get_weeks()
+    image = Image.open(".\\tempimages\\file.png")
+    image.save(".\\images\\"+str(year)+"-"+str(week)+".gif")
+    
+
+    plt.show()'
+    '''
---
+++ DesktopTools.py
@@ -0,0 +1,304 @@
+﻿import DataProcessing as dp
+import logging.handlers,logging
+import tkinter
+import tkinter.messagebox as msg
+import tkinter.ttk as ttk
+import json
+import time
+import os
+import glob
+import PIL
+#日志部分 
+
+logger = logging.getLogger("app_logger")
+logger.setLevel(logging.INFO)
+#创建一个StreamHandler
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+logger.addHandler(stream_handler)
+#创建Formatter
+formatter = logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+#创建TimedRotatingFileHandler
+formatted_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
+filehandler = logging.handlers.TimedRotatingFileHandler("logs\\"+formatted_time+".log")
+filehandler.setFormatter(formatter)
+logger.addHandler(filehandler)
+
+year,week = dp.get_weeks()
+DefaultCityCode = 101010100
+dp.data_7day_to_image(DefaultCityCode)
+
+
+
+#读取城市代码文件
+#cities=[]
+pros=[]
+pro2 = {}
+cities=[]
+citydict={}
+with open("citycode.json",'r') as f:
+    citydict=json.load(f)
+    
+    for value in range(len(citydict["城市代码"])):
+
+        pros.append(citydict["城市代码"][value]["省"])
+        pro2[citydict["城市代码"][value]["省"]]=value
+        #for item in value["市"]:
+        #    cities.append(item["市名"])
+
+    logger.info("城市代码加载成功")
+    
+'''
+root = tkinter.Tk()
+root.geometry()
+root.mainloop()
+'''
+#加载图片文件
+def load_icon():
+    image_dict={}
+    cwd = os.getcwd()
+    imagefiles = glob.glob("data\\*")
+    for item in imagefiles:
+        deitem = item[5:-4]
+        image = tkinter.PhotoImage(file=item)
+        image_dict[deitem] = image
+        logger.info("已加载图片:"+deitem)
+    
+    return image_dict
+
+#获取当日天气情况
+def get_today_weather_inf(code):
+    weather = dp.parse_html_1day(code)
+    
+    
+    return weather
+
+#获取7日天气情况
+def get_weather_col(code):
+    html = dp.get_html(dp.url_7day+str(code)+".shtml")
+    pre_weather = dp.parse_html_7day(html)
+    weather=[]
+    
+    for item in pre_weather:
+        
+        #weather.append(item["date"])
+        #weather.append(item["weather"])
+        #weather.append(item["temperature"])
+        
+        weather.append([item["date"],item["weather"],item["temperature"]+'℃'])
+    #weather_df = pd.DataFrame(weather, columns=[ "日期","天气", "温度"])
+    #print(type(weather_df))
+    
+    return weather
+
+def province_on_select(event):
+    global table
+    #print(type(event))
+    #print(type(ProvinceComboBox.get()))
+    cities=[]
+    for item in citydict["城市代码"][pro2[ProvinceComboBox.get()]]["市"]:
+        cities.append(item["市名"])
+    CityComboBox["value"] = cities
+    CityComboBox.current(0)
+    CityComboBox.update()
+
+
+    citycode = citydict["城市代码"][pro2[ProvinceComboBox.get()]]["市"][CityComboBox["value"].index(CityComboBox.get())]["编码"]
+    logger.info("城市代码:"+str(citycode))
+    new_today = get_today_weather_inf(citycode)
+    new_7day = get_weather_col(citycode)
+    logger.info("天气信息:"+str(new_today))
+    
+    if new_7day != None:
+        for item in weathertable.get_children():
+            weathertable.delete(item)
+        for index,data in enumerate(new_7day):
+            weathertable.insert('',tkinter.END,values=data)
+    weathertable.update()
+    logger.info("7日天气数据重载成功")
+      
+    tablepath = dp.data_7day_to_image(citycode)
+    table = tkinter.PhotoImage(file=tablepath)  
+    tablelabel.config(image = table)  
+    #重新设置天气信息
+    #weathericon.set()
+    weathericon.config(image = image_dict[new_today["weather"]])
+    weatherinfo["text"] = "现在天气:"+new_today["weather"]
+    temperature["text"] = "温度 " + new_today["high"] + " " + new_today["low"]
+    pm["text"] = "pm2.5:" + str(new_today["pm25"]) + " pm10:" + str(new_today["pm10"])
+    wind["text"] = new_today["fx"]+" "+new_today["fl"]
+    air["text"] = "空气质量" + " " + str(new_today["aqi"]) + " " + new_today["air"]
+    sunrise["text"] = "日出时间 " + new_today["sunrise"]
+    sunset["text"] = "日落时间 " + new_today["sunset"]
+    moi["text"] = "相对湿度 " + new_today["moicon"]
+    notice["text"] = new_today["tip"]
+
+    tablelabel.update()
+
+    weathericon.update()
+    weatherinfo.update()
+    temperature.update()
+    pm.update()
+    wind.update()
+    air.update()
+    sunrise.update()
+    sunset.update()
+    moi.update()
+    notice.update()
+
+    logger.info("天气数据重载成功")
+
+
+    #root.update()
+
+    
+
+def city_on_select(event):
+    global table
+    #print(type(event))
+    if CityComboBox.get() == None or ProvinceComboBox.get() == None:
+        ProvinceComboBox.current(0)
+        cities=[]
+        for item in citydict["城市代码"][pro2[ProvinceComboBox.get()]]["市"]:
+            cities.append(item["市名"])
+        CityComboBox["value"]=cities
+        CityComboBox.current(0)
+        return False
+    citycode = citydict["城市代码"][pro2[ProvinceComboBox.get()]]["市"][CityComboBox["value"].index(CityComboBox.get())]["编码"]
+    #print(citycode)  
+    logger.info("城市代码:"+str(citycode))
+    new_today = get_today_weather_inf(citycode)
+    new_7day = get_weather_col(citycode)
+    '''
+    if new_7day or new_today == {}:
+        logger.info("无法获取天气信息")
+        result = msg.showerror("错误","未提供该区域天气信息")
+        print(f"错误:{result}")
+    else:
+        logger.info("天气信息:"+str(new_today))
+    #print(new_today)
+    '''
+    logger.info("天气信息:"+str(new_today))
+    if new_7day != None:
+        for item in weathertable.get_children():
+            weathertable.delete(item)
+        for index,data in enumerate(new_7day):
+            weathertable.insert('',tkinter.END,values=data)
+    weathertable.update()
+    logger.info("7日天气数据重载成功")
+ 
+    tablepath = dp.data_7day_to_image(citycode)
+    table = tkinter.PhotoImage(file=tablepath)
+    #重新设置天气信息  
+    #weathericon.set()
+    tablelabel.config(image = table)
+
+    weathericon.config(image = image_dict[new_today["weather"]])
+    weatherinfo["text"] = "现在天气:"+new_today["weather"]
+    temperature["text"] = "温度 " + new_today["high"] + " " + new_today["low"]
+    pm["text"] = "pm2.5:" + str(new_today["pm25"]) + " pm10:" + str(new_today["pm10"])
+    wind["text"] = new_today["fx"]+" "+new_today["fl"]
+    air["text"] = "空气质量" + " " + str(new_today["aqi"]) + " " + new_today["air"]
+    sunrise["text"] = "日出时间 " + new_today["sunrise"]
+    sunset["text"] = "日落时间 " + new_today["sunset"]
+    moi["text"] = "相对湿度 " + new_today["moicon"]
+    notice["text"] = new_today["tip"]
+
+    tablelabel.update()
+
+    weathericon.update()
+    weatherinfo.update()
+    temperature.update()
+    pm.update()
+    wind.update()
+    air.update()
+    sunrise.update()
+    sunset.update()
+    moi.update()
+    notice.update()
+
+    logger.info("天气数据重载成功")
+    #root.update()
+
+
+#创建窗口及配置
+root = tkinter.Tk()
+root.geometry('1401x500')
+root.resizable(0, 0)
+#root.attributes('-alpha', 0.4)
+
+image_dict = load_icon()
+
+#
+#customstyle = ttk.Style()
+#customstyle.configure('Custom.Toplevel',background="#F0F0F0")
+
+#root.attributes("-transparent", "gray")
+#设置标题
+root.title("天气")
+
+#表格图片加载
+table = tkinter.PhotoImage(file=".\\images\\"+str(year)+"-"+str(week)+"-"+str(DefaultCityCode)+".gif")
+tablelabel = ttk.Label(root,image=table)
+tablelabel.place(x=801,y=5)
+
+
+
+
+weather = get_weather_col(DefaultCityCode)
+logger.info("默认城市7日天气数据获取成功")
+#7日天气表格
+weathertable = ttk.Treeview(root,height=7,columns=["日期","天气","温度"],show='headings')
+#label = ttk.Label(root,text=get_weather_col()).pack()
+weathertable.heading("日期",text="日期")
+weathertable.heading("天气",text="天气")
+weathertable.heading("温度",text="温度")
+
+weathertable.column("日期",width=266)
+weathertable.column("天气",width=266)
+weathertable.column("温度",width=266)
+
+#循环插入数据
+for index,data in enumerate(weather):
+    weathertable.insert('',tkinter.END,values=data)
+
+weathertable.place(x=0,y=300)
+logger.info("7日天气已成功加载")
+
+#获取当日数据
+today = get_today_weather_inf(DefaultCityCode)
+logger.info("默认城市天气数据获取成功")
+
+#当日天气
+ProvinceComboBox = ttk.Combobox(root,value=pros)
+ProvinceComboBox.current(0)
+ProvinceComboBox.bind('<<ComboboxSelected>>', province_on_select)
+ProvinceComboBox.place(x=10,y=5)
+logger.info("省份加载成功")
+
+for item in citydict["城市代码"][pro2[ProvinceComboBox.get()]]["市"]:
+    cities.append(item["市名"])
+CityComboBox = ttk.Combobox(root,value=cities)
+CityComboBox.current(0)
+CityComboBox.bind('<<ComboboxSelected>>', city_on_select)
+CityComboBox.place(x=300,y=5)
+logger.info("城市加载成功")
+
+#city = ttk.Label(root,text=today["city"]+"天气",font=("黑体",15)).place(x=10,y=5)
+weathericon = ttk.Label(root,image=image_dict[today["weather"]]);weathericon.place(x=0,y=40)
+weatherinfo = ttk.Label(root,text="现在天气:"+today["weather"],font=("黑体",20));weatherinfo.place(x=91,y=55)
+temperature = ttk.Label(root,text="温度"+" "+today["high"]+" "+today["low"],font=("黑体",15));temperature.place(x=91,y=95)
+pm = ttk.Label(root,text="pm2.5:"+str(today["pm25"])+" "+"pm10:"+str(today["pm10"]),font=("黑体",15));pm.place(x=91,y=131)
+wind = ttk.Label(root,text=today["fx"]+" "+today["fl"],font=("黑体",15));wind.place(x=91,y=171)
+sevendays = ttk.Label(root,text="七日天气预报",font=("黑体",15)).place(x=5,y=256)
+
+air = ttk.Label(root,text="空气质量" + " " + str(today["aqi"]) + " " + today["air"],font=("黑体",15));air.place(x=450,y=55)
+sunrise = ttk.Label(root,text="日出时间" + " " + today["sunrise"],font=("黑体",15));sunrise.place(x=450,y=90)
+sunset = ttk.Label(root,text="日落时间" + " " + today["sunset"],font=("黑体",15));sunset.place(x=450,y=120)
+moi = ttk.Label(root,text="相对湿度" + " " + today["moicon"],font=("黑体",15));moi.place(x=450,y=150)
+notice = ttk.Label(root,text=today["tip"],font=("黑体",10));notice.place(x=450,y=185)
+logger.info("单日天气基本控件加载成功")
+
+root.mainloop()
